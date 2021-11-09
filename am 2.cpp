969d1b619718adce243d9c4823dec1d4ab18923e
nhat150923 <nhatviptk003@gmail.com>
2021-11-09 15:56:16 +0700

    Create am 2.cpp

---
+++ am 2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+ int a ;
+	printf("nhap so nguyen a");
+	scanf("%d",&a);
+if (a % 2 == 0)
+ printf("a La So Chan");
+      else printf("a La So Le");
+if (a>=0) printf(", Duong");
+      else printf(", Am");
+      }
